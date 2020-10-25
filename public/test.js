7fcd438809535f42f7b4001caadc75be9a74d77b
Steven Torrence <storrence88@gmail.com>
2020-10-25 14:46:03 -0500

    Function types and Void

--- public/test.js
+++ public/test.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log('Testing 1-2-3');
+// ==============
+// Function Types
+// ==============
+// Just like our other variables, we can declare a Function (with a capital F) type
+var greet;
+// In the future, we won't be able to set the variable greet to anything other than a function
+// None of these will work:
+// greet = 'Hello!'; // Type 'string' is not assignable to type 'Function'.
+// greet = 12; // Type 'number' is not assignable to type 'Function'.
+// This will work
+greet = function () {
+    console.log('Hello world!');
+};
+// ----- Function Parameters Types -----
+// We can explicitly set the types for each of the function parameters
+// We can also set an optional parameter using the "?"
+// Lastly, we can set a default value as the last parameter for the function
+var add = function (a, b, c) {
+    if (c === void 0) { c = 20; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10);
+// ----- Function Return Type and Void -----
+// We can explicitly set a function to return a certain type
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 7);
+// If a function doesn't return anything, we can set its type to void
+var multiply = function (a, b) {
+    console.log(a * b);
+};
--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,40 @@
-console.log('Testing 1-2-3');
+// ==============
+// Function Types
+// ==============
+
+// Just like our other variables, we can declare a Function (with a capital F) type
+let greet: Function;
+
+// In the future, we won't be able to set the variable greet to anything other than a function
+// None of these will work:
+// greet = 'Hello!'; // Type 'string' is not assignable to type 'Function'.
+// greet = 12; // Type 'number' is not assignable to type 'Function'.
+
+// This will work
+greet = () => {
+  console.log('Hello world!');
+};
+
+// ----- Function Parameters Types -----
+// We can explicitly set the types for each of the function parameters
+// We can also set an optional parameter using the "?"
+// Lastly, we can set a default value as the last parameter for the function
+const add = (a: number, b: number, c: number | string = 20) => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(5, 10);
+
+// ----- Function Return Type and Void -----
+// We can explicitly set a function to return a certain type
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(10, 7);
+
+// If a function doesn't return anything, we can set its type to void
+const multiply = (a: number, b: number): void => {
+  console.log(a * b);
+};
